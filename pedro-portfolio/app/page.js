1274e3b5e702fd1d271c4f7b383bcd0ce21500ac
Pedro Mota Mendes <pepeumotinha@outlook.com>
2025-09-17 13:25:33 -0300

    base page and socials component done

--- pedro-portfolio/app/page.js
+++ pedro-portfolio/app/page.js
@@ -1,95 +1,28 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import Socials from "../components/Socials";
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main className={styles.container}>
+      <Image
+        src="/myimage.jpg"
+        alt="Foto de perfil"
+        width={200}
+        height={200}
+        className={styles.avatar}
+      />
+      <h1 className={styles.title}>Pedro Mota Mendes</h1>
+      <p className={styles.description}>
+        Estudante do curso de Ciencias da Computação na Universidade Catolica de
+        Pernambuco. Sou apaixonado por tecnologia, e principalmente por
+        desenvolvimento web. Tenho experiencia em HTML, CSS, Javascript e React,
+        além de conhecimentos em linguagens como C, Java e Python. Meu trunfo é
+        a minha adaptabilidade e vontade de aprender coisas novas, além de ter
+        boa proatividade e facilidade de trabalhar em equipe.
+      </p>
+      <div className={styles.socials}>
+        <Socials />
+      </div>
+    </main>
   );
 }
---
+++ pedro-portfolio/components/Socials.js
@@ -0,0 +1,29 @@
+import styles from "./socials.style.css";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+export default function Socials() {
+  return (
+    <div>
+      <h2>Redes Sociais</h2>
+      <ul className="socials">
+        <li>
+          <a
+            href="https://github.com/pep287"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/pedro-mota-mendes-011362328"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
